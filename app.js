696c47e1014e2b583a387dfd621bd0b3ab2ce9fd
Jal Panchal <jal08panchal@gmail.com>
2019-04-13 08:31:32 -0700

    Setting basic routes

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+
+app.get("/", (req,res)=> {
+    res.send("Index")
+})
+
+
+app.get("/about", (req,res) => {
+    res.send("about")
+})
+
+
 app.listen(port, () => {
-    console.log(`Server is listening at ${port}`)
+    console.log(`Segrver is listening at ${port}`)
 })
