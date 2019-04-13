2c160a1304a0e57835e737116a4d6c874d56f864
Jal Panchal <jal08panchal@gmail.com>
2019-04-13 13:01:37 -0700

    Adding bodyParser

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const mongoose = require("mongoose");
 const Idea = require("./models/Myjot")
+const bodyParser = require("body-parser");
 const port = 5000;
 mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://localhost/myjot", {useNewUrlParser: true})
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get("/ideas/add", (req,res)=> {
     res.render("ideas/add")
+})
+
+app.post("/ideas", (req,res)=> {
+    console.log(req.body)
 })
 app.get("/", (req,res)=> {
     res.render("index")
