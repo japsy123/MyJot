7058cebddf4da2bca443bb3e27c821f0153ec4de
Jal Panchal <jal08panchal@gmail.com>
2019-04-13 08:51:14 -0700

    Setting up handlebars

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+var exphbs  = require('express-handlebars');
+
 const port = 5000;
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get("/", (req,res)=> {
     res.send("Index")
