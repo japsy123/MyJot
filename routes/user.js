8f01a28f7b47ed8d56cfaf7528f32524b4519239
Jal Panchal <jal08panchal@gmail.com>
2019-04-14 11:46:14 -0700

    Adding server side validation for register

--- routes/user.js
+++ routes/user.js
@@ -10,4 +10,28 @@ router.get("/register", (req,res) => {
     res.render("users/register")
 })
 
+router.post("/user/register", (req,res) => {
+    const errors = [];
+    console.log(req.body.password.length)
+
+    if(req.body.password !== req.body.password2) {
+        errors.push({
+            text: "Password dont match"
+        })
+    }
+    if(req.body.password.length < 4) {
+        errors.push({
+            text: "Please enter larger password"
+        })
+    }
+    if(errors.length > 0 ){
+        res.render("users/register", {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        })
+    }
+})
 module.exports = router;
