4b760a293b1f277f825f67a3d81edae8de5810fb
Jal Panchal <jal08panchal@gmail.com>
2019-04-14 14:17:56 -0700

    Completing user registration with bcryptjs

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Idea = require("../models/Myjot");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const User = require("../models/User");
 
 router.get("/login", (req,res) => {
     res.render("users/login")
@@ -32,6 +35,37 @@ router.post("/user/register", (req,res) => {
             password: req.body.password,
             password2: req.body.password2
         })
+    } else {
+        User.findOne({email: req.body.email})
+        .then(user => {
+            if(user) {
+                req.flash("error_msg", "Email already registered!");
+                res.redirect("/register")
+            }
+            else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                });
+                bcrypt.genSalt(10, (err,salt)=> {
+                    bcrypt.hash(newUser.password, salt, (err,hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                        .then(user => {
+                            req.flash("success_msg", "You are now registered");
+                            res.redirect("/login")
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            return;
+                        })
+                    })
+                })
+            }
+        })
+        
     }
 })
 module.exports = router;
